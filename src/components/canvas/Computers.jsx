b5a698d1e4be25e4687ed26f6578903d33faa32c
Ziyad Hammad <ziyadhammad0@gmail.com>
2023-03-27 21:21:40 -0400

    Hero Section Complete, starting About Section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   // const computer = useGLTF('../../../public/desktop_pc/scene.gltf');
   const computer = useGLTF('./desktop_pc/scene.gltf');
 
@@ -23,8 +23,8 @@ const Computers = () => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        // scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.5 : 0.75}
+        position={isMobile ? [0, -2, -1.5] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -32,7 +32,30 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
-  <Canvas
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+
+    // Set the initial value of the 'isMobile' state variable
+    setIsMobile(mediaQuery.matches)
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+    // Add the callback function as a listener for changes to the mediaQuery
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
+  
+
+  return (
+    <Canvas
     frameloop="demand"
     shadows
     camera={{ position: [20, 3, 5], fov: 25 }}
@@ -44,10 +67,10 @@ const ComputersCanvas = () => {
         maxPolarAngle={Math.PI / 2}
         minPolarAngle={Math.PI / 2}
       />
-      <Computers />
+      <Computers isMobile={isMobile} />
     </Suspense>
     <Preload all />
-  </Canvas>;
-};
+    </Canvas>   
+)};
 
 export default ComputersCanvas;
