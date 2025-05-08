c34fb4f2b143781cb699dd9008a52f02e47d6fac
Ziyad Hammad <ziyadhammad0@gmail.com>
2025-05-08 16:51:13 -0400

    Updated Resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
           <li
             className='text-secondary hover:text-white text-[18px] font-medium cursor-pointer'
               >
-            <a href="https://drive.google.com/file/d/15TR9C5VlEif2I6WxfKB_nHgiVleAOJ0X/view?usp=sharing" target="_blank">
+            <a href="https://drive.google.com/file/d/1LAabYqRq8e_3c59olB9cuWHHmfE3oTB-/view?usp=drive_link" target="_blank">
               Resume
             </a>
             </li>
